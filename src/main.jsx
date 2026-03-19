09a32afb87521a233c9f1144530b1f3bf99f1548
SakshamWalia03 <wsaksham37@gmail.com>
2026-03-19 16:00:19 +0530

    FIxed issue related to deleted package

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "./store/store";
 import App from "./App.jsx";
 import "./styles/global.scss";
